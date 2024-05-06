eea70c209569a13141eee8cb9db494e7db0e195b
Spewedandbraked <samogon4ik365@gmail.com>
2024-05-07 02:51:34 +0400

    удалена строка

--- app/Http/Controllers/BucketController.php
+++ app/Http/Controllers/BucketController.php
@@ -12,15 +12,13 @@ class BucketController extends Controller
     {
         $order = Order::firstOrCreate(['customer' =>  $id],['track' => uuid_create()]);
         return view('bucket',[                                                                                                                                                                                                                                                                                                                                               //история: однажды студентишку потянуло в романтику и потом он восемь месяцев (а возможно и больше(покажет время)) в лютом угаре прибывал. мораль - надо было учиться и относиться ко всем одинаково плохо, а не делать исключения и нарываться на приключения. никогда в жизни я больше не повторю произошедшего. один раз подпустишь человека к себе близко - потом всех остальных возненавидишь на остаток времен (в данном случае на два курса обучения, благо я больше не увижу никого из них). доманипулировался, доигрался. виноват сам и ничего не поделать 
-            'orderedprods' => orderedprod::all(), 
-            'orders' => Order::all(),
+            'orderedprods' => orderedprod::all(),
         ]);
     }
     public function add($Userid, $Postid){
         $orderedprod = new orderedprod;
         $orderedprod->product_id = $Postid;
-        $temp = Order::firstOrCreate(['customer' =>  $Userid],['track' => uuid_create()]);
-        $orderedprod->order_id = $temp->id;
+        $orderedprod->order_id = Order::firstOrCreate(['customer' =>  $Userid],['track' => uuid_create()])->id;
 
         $orderedprod->save();
         return redirect('products');
